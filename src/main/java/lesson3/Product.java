a88e3bf249efa678183d4da13f85c9ed868ad740
Vladimir <bubleeff@yandex.ru>
2025-04-10 00:34:40 +0300

    add prudct and park clssses

--- src/main/java/lesson3/Product.java
+++ src/main/java/lesson3/Product.java
@@ -1,6 +1,6 @@
 package lesson3;
 
-public class Product {    // Поля класса
+public class Product {
     private String name;
     private String dateProduction;
     private String manufacturer;
@@ -8,7 +8,6 @@ public class Product {    // Поля класса
     private double price;
     private boolean bookedByTheBuyer;
 
-    // Конструктор класса
     public Product(String name, String dateProduction, String manufacturer,
                    String сountryOfOrigin, double price, boolean bookedByTheBuyer) {
         this.name = name;
@@ -18,8 +17,6 @@ public class Product {    // Поля класса
         this.price = price;
         this.bookedByTheBuyer = bookedByTheBuyer;
     }
-
-    // Метод для вывода информации об объекте в консоль
     public void getInformation() {
         System.out.println("Название товара: " + name);
         System.out.println("Дата производства: " + dateProduction);
@@ -33,7 +30,6 @@ public class Product {    // Поля класса
         }
     }
     public static void main(String[] args) {
-        // Создание экземпляра класса Товар
         Product products = new Product(
                 "Кубанская бурёнка",
                 "01-01-2025",
@@ -42,9 +38,6 @@ public class Product {    // Поля класса
                 94.0,
                 true
         );
-
-        // Вывод информации о товаре
-        products.getInformation();
 
         Product[] productsArray = new Product[5];
         productsArray[0] = new Product("Веселый молочник", "01-02-2025", "ООО Завтра", "Россия", 100.0, false);
